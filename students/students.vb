50481709074c8e12df895ebb4616f8533aa1d2ea
Isomorphic-Knots <35407630+Isomorphic-Knots@users.noreply.github.com>
2018-05-27 21:48:42 -0400

    Add files via upload

---
+++ students/students.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Class STUDENT
+        Public firstname As String
+        Public lastname As String
+        Public dob As Date
+
+    End Class
+
+    Dim students(9) As STUDENT
+    Dim studentCount As Integer = 0
+    Sub Main()
+
+        For Each element As STUDENT In students
+            element = New STUDENT
+            studentCount += 1
+        Next
+
+
+    End Sub
+
+End Module
