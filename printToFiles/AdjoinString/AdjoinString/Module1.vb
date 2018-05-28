380795116219f594f06e55a730bddff2913bd64a
Isomorphic-Knots <35407630+Isomorphic-Knots@users.noreply.github.com>
2018-05-27 21:47:03 -0400

    Add files via upload

---
+++ printToFiles/AdjoinString/AdjoinString/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Function JoinStrings(strings() As String) As String
+
+        If strings.Length = 0 Then
+            Console.WriteLine("String array is empty!")
+            Return Nothing
+        Else
+            Dim result As String = strings(0)
+            For i = 1 To strings.Length - 1
+                result &= " " & strings(i)
+            Next
+
+            Return result
+        End If
+
+
+    End Function
+
+
+    Sub Main()
+
+        Dim Str As String
+        Dim stringz As New List(Of String)
+
+        Do
+            Console.Write("Please enter a sentence (ENTER to exit): ")
+            Str = Console.ReadLine
+
+            If Str <> "" Then
+                stringz.Add(Str)
+            End If
+
+        Loop Until Str = ""
+
+        Console.WriteLine(JoinStrings(stringz.ToArray))
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ printToFiles/printToFiles/printToFiles/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim numbers As New List(Of Integer)
+        Dim summation As Integer
+        Dim filenum = FreeFile()
+
+        'Openmode to start new file if it doesnt exist
+        'Append to add to a file that does exist
+        'here we use open mode
+        FileOpen(filenum, "testfile.txt", OpenMode.Output)
+
+        For i = 0 To 100
+
+            numbers.Add(i)
+        Next
+
+        PrintLine(filenum, "Number of list elements: " & numbers.Count)
+        Print(filenum, "list of elements: ")
+
+        For i = 0 To numbers.Count - 1
+            Print(filenum, numbers(i) & " ")
+
+            summation = numbers(i) + summation
+        Next
+
+        PrintLine(filenum, " ")
+        PrintLine(filenum, "Summation of all integers: " & summation)
+
+        FileClose(filenum)
+
+        'tests for input to console for output'
+        FileOpen(filenum, "testfile.txt", OpenMode.Input)
+        While Not EOF(filenum)
+            Console.WriteLine(LineInput(filenum))
+            Console.ReadLine()
+        End While
+        FileClose(filenum)
+
+
+    End Sub
+
+End Module
